21dfb926d5543e1b063644736bf992ad1bb29f16
Linghui Zeng <lh.zeng@hotmail.com>
2017-02-23 09:25:37 +0800

    Update clear_cloudify.sh

--- install/clear_cloudify.sh
+++ install/clear_cloudify.sh
@@ -4,20 +4,24 @@
 # nova keypair-delete agent-kp
 # nova keypair-delete manager-kp
 
+set -x
+set -e
+
 instances=`nova list | grep 'server_clearwater-test_' | awk '{print $4}'`
 for instance in $instances
 do  
-    echo "Delete vIMS instance ${instance}"
+    # Delete vIMS instance
     nova delete $instance
 done
 
 clearwatersgs="internal_sip ralf sprout base bono bind homestead ellis homer"
 for service in $clearwatersgs
 do 
-    echo "Delete security group clearwater-sg_${service}"
     neutron security-group-delete clearwater-sg_${service}
 done
 
-
 neutron security-group-list
 nova list
+
+set +e
+set +x
